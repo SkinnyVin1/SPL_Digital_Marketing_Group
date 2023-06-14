b9a3cdd5e03753ae61d2346e880888a97562ae73
Skinny_Vin <118040316+SkinnyVin1@users.noreply.github.com>
2023-06-14 15:05:05 +0800

    navbar

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const navBar = document.querySelector('.nav-bar');
 
+
 window.addEventListener('scroll', () => {
   if (window.scrollY > 100) {
     navBar.classList.add('scrolled');
+    
   } else {
     navBar.classList.remove('scrolled');
   }
